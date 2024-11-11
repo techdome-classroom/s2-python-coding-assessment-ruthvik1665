2acadbc3f1d29d42b00eeedf9339b4300384439e
Ruthvik Basetti <ruthvikbasetty5@gmail.com>
2024-11-11 14:53:07 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,6 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        
+        return not stack
         pass
 
 
